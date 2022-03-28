bc6a557fd221aec7e41af79833d1b796e32814a5
PainfullyBad <10093257@edu.regiocollege.nl>
2022-03-28 11:45:07 +0200

    ....

---
+++ project/new/pychart.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+<h1>hallo hier de diagrammen</h1>
+</body>
+</html>
